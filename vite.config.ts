8ec812e5de9981f625d811271af55dc59abb405c
rulf-b <ibrakg14172@gmail.com>
2025-06-20 21:18:31 +0300

    hatalı

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path" // Bu satır eklendi
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Aşağıdaki "resolve" bloğu eklendi
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 })
